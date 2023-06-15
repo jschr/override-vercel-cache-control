c642e54aac685fabd40df7227d3024c8bfa8e36c
Jordan <github@jschr.io>
2023-06-15 15:45:32 -0400

    Set max-age=1

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "s-maxage=1, stale-while-revalidate=59",
+            value: "max-age=1",
           },
         ],
       },
